2a7cd4a438eddc9f5552801e5a31aa8010db3929
Krishankumar85 <krishan.kumar1@hashedin.com>
2020-04-02 08:44:28 +0530

    Added forgot password
    (cherry picked from commit bece89126f8496339b4a0db39265415881d03f54)
    
    # Conflicts:
    #	app/src/main/java/in/bitspilani/eon/presentation/auth/fragments/SignInFragment.kt

--- app/src/main/java/in/bitspilani/eon/presentation/auth/fragments/SignInFragment.kt
+++ app/src/main/java/in/bitspilani/eon/presentation/auth/fragments/SignInFragment.kt
@@ -2,19 +2,22 @@ package `in`.bitspilani.eon.presentation.auth.fragments
 
 
 import `in`.bitspilani.eon.R
-import `in`.bitspilani.eon.utils.getViewModelFactory
-import `in`.bitspilani.eon.viewmodel.AuthViewModel
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
-
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class SignInFragment : Fragment() {
 
-    private val authViewModel by viewModels<AuthViewModel> { getViewModelFactory() }
+    lateinit var username: EditText
+    lateinit var password: EditText
+    lateinit var buttonLogin: Button
+    lateinit var forgotPassword: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,5 +27,33 @@ class SignInFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_sign_in, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        username = view.findViewById(R.id.etEmailAddress)
+        password = view.findViewById(R.id.etPassword)
+        buttonLogin = view.findViewById(R.id.btn_login)
+        forgotPassword = view.findViewById(R.id.tv_forgot_password)
+
+        buttonLogin.setOnClickListener(View.OnClickListener {
+
+            // getting input username and password
+            val username = username.text.toString().trim();
+            val password = password.text.toString().trim();
+
+            // to do validation
+
+            // demo of getting data from user - need to remove
+            Toast.makeText(getActivity(),"Username: "+username+"Password: "+password,Toast.LENGTH_SHORT).show();
+        })
+
+        forgotPassword.setOnClickListener(View.OnClickListener {
+
+            // to do
+
+            // click event on forgot password
+            Toast.makeText(getActivity(),"Forgot password",Toast.LENGTH_SHORT).show();
+        })
+
+    }
 }
