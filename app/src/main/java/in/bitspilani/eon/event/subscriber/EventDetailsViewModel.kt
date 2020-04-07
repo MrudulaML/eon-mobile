a68f5942d9308d2c5e8fa42a94f455e9e1e5241a
Milind Dave <milind_dave@bounceshare.com>
2020-04-07 17:34:44 +0530

    event detail srceen and bottom seat ui created

---
+++ app/src/main/java/in/bitspilani/eon/event/subscriber/EventDetailsViewModel.kt
@@ -0,0 +1,7 @@
+package `in`.bitspilani.eon.event.subscriber
+
+import androidx.lifecycle.ViewModel
+
+class EventDetailsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/in/bitspilani/eon/event/subscriber/eventDetails.kt
@@ -0,0 +1,33 @@
+package `in`.bitspilani.eon.event.subscriber
+
+import `in`.bitspilani.eon.R
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class eventDetails : Fragment() {
+
+    companion object {
+        fun newInstance() = eventDetails()
+    }
+
+    private lateinit var viewModel: EventDetailsViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.event_details_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(EventDetailsViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
