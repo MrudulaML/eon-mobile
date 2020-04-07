f1f4267fa58d5da41663f54f30c76673a7ba8b9b
o0ashutosh0o <Qazxsw@39>
2020-04-07 17:30:43 +0530

    adds events ui 1.3

---
+++ app/src/main/java/in/bitspilani/eon/eventOrganiser/ui/InviteesAdapter.kt
@@ -0,0 +1,40 @@
+package `in`.bitspilani.eon.eventOrganiser.ui
+
+import `in`.bitspilani.eon.databinding.InviteeItemRowBinding
+import `in`.bitspilani.eon.eventOrganiser.data.Invitee
+import `in`.bitspilani.eon.generated.callback.OnClickListener
+import android.R
+import android.view.LayoutInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.widget.PopupMenu
+import androidx.recyclerview.widget.RecyclerView
+
+
+class InviteesAdapter(private val evenList : List<Invitee>): RecyclerView.Adapter<InviteesAdapter.InviteesViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): InviteesViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = InviteeItemRowBinding.inflate(inflater)
+        return InviteesViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int = evenList.size
+
+    override fun onBindViewHolder(holder: InviteesViewHolder, position: Int) = holder.bind(evenList[position])
+
+    inner class InviteesViewHolder(private val binding: InviteeItemRowBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: Invitee) {
+
+            binding.invitee = item
+            binding.executePendingBindings()
+
+
+
+
+        }
+
+
+    }
+
+}
