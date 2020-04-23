5333052b774a984cc23433137faf889278980e3a
Milind Dave <milind_dave@bounceshare.com>
2020-04-23 09:36:56 +0530

    facebook external link added in code

--- app/src/main/java/in/bitspilani/eon/event_organiser/ui/PagerEventFragment.kt
+++ app/src/main/java/in/bitspilani/eon/event_organiser/ui/PagerEventFragment.kt
@@ -213,8 +213,6 @@ class PagerEventFragment(private val eventDetailResponse: DetailResponseOrganise
         if (ShareDialog.canShow(ShareLinkContent::class.java)) {
 
             val shareLinkContent = ShareLinkContent.Builder()
-                .setContentTitle("Share text on facebook")
-                .setQuote("This is useful link")
                 .setContentUrl(Uri.parse(eventDetailResponse.data.external_links))
                 .build()
             shareDialog?.show(shareLinkContent)
