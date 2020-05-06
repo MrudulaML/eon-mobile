1d49f555bd6e42ae90a469373a55d5124c2c0f30
Krishankumar85 <krishan.kumar1@hashedin.com>
2020-05-06 17:50:40 +0530

    Fixed login page scrollable

--- app/src/main/java/in/bitspilani/eon/login/ui/SignInFragment.kt
+++ app/src/main/java/in/bitspilani/eon/login/ui/SignInFragment.kt
@@ -9,6 +9,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -27,7 +28,7 @@ class SignInFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
+        activity?.getWindow()?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
         return inflater.inflate(R.layout.fragment_sign_in, container, false)
     }
 
